c39eafb7a1cf14fa45952dc4d00acf119776a138
AlvaroTrindade <alvaro.trindade@outlook.com>
2022-06-05 21:01:49 -0300

    commit alvaro

--- HomePage/hp.js
+++ HomePage/hp.js
@@ -4,3 +4,48 @@ function classToggle() {
   }
   
   document.querySelector('#toggle-animation').addEventListener('click', classToggle);
+
+  $(document).ready(function(){
+    var zindex = 10;
+    
+    $("div.card_mural").click(function(e){
+      e.preventDefault();
+  
+      var isShowing = false;
+  
+      if ($(this).hasClass("show")) {
+        isShowing = true
+      }
+  
+      if ($("div.cards_mural").hasClass("showing")) {
+        // a card is already in view
+        $("div.card_mural.show")
+          .removeClass("show");
+  
+        if (isShowing) {
+          // this card was showing - reset the grid
+          $("div.cards_mural")
+            .removeClass("showing");
+        } else {
+          // this card isn't showing - get in with it
+          $(this)
+            .css({zIndex: zindex})
+            .addClass("show");
+  
+        }
+  
+        zindex++;
+  
+      } else {
+        // no cards in view
+        $("div.cards_mural")
+          .addClass("showing");
+        $(this)
+          .css({zIndex:zindex})
+          .addClass("show");
+  
+        zindex++;
+      }
+      
+    });
+  });
